7fbf23bbb2fbea781921e3c38e735e81b31dbc3b
Gowtham <gowtham@cloco.com.au>
2026-08-27 21:03:11 +0530

    builde error fixed

--- components/ResultsShowcase.tsx
+++ components/ResultsShowcase.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 import styles from "./ResultsShowcase.module.css";
 
-const cardVariants = {
+const cardVariants: Variants = {
   hidden: { opacity: 0, y: 30 },
   visible: { 
     opacity: 1, 
