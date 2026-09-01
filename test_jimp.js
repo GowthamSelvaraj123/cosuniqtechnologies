2de91cec964c6103bfee489ad13b8e05f772c5e3
Gowtham <gowtham@cloco.com.au>
2026-09-01 11:33:08 +0530

    deleted unwanted files

--- test_jimp.js
+++
@@ -1,2 +0,0 @@
-const jimp = require('d:\\fullstack\\cosuniqtechnologies\\node_modules\\jimp');
-console.log(Object.keys(jimp));
--- update_story.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = 'd:\\fullstack\\cosuniqtechnologies\\components\\ServicesPage.tsx';
-let content = fs.readFileSync(path, 'utf8');
-
-// Replace all instances of the 3D images with the single strategy_story image
-content = content.replace(/<img src="\/assets\/services\/[a-zA-Z0-9_]+_nobg\.png" alt="Service Visual" style=\{\{ width: '100%', height: 'auto', borderRadius: '16px' \}\} \/>/g, 
-    `<img src="/assets/services/strategy_story.jpg" alt="Service Story" style={{ width: '100%', height: 'auto', borderRadius: '16px', mixBlendMode: 'multiply' }} />`);
-
-fs.writeFileSync(path, content);
